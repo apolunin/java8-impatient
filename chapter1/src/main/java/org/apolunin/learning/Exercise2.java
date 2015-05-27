d844786cedc8afe0dd6e2c9e9b33629a41a6752a
Andrii Polunin <apolunin@gmail.com>
2015-05-27 18:27:37 +0300

    implement exercise 2 from chapter 1

---
+++ chapter1/src/main/java/org/apolunin/learning/Exercise2.java
@@ -0,0 +1,41 @@
+package org.apolunin.learning;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Using the listFiles(FileFilter) and isDirectory methods of the java.io.File class,
+ * write a method that returns all subdirectories of a given directory.
+ * Use a lambda expression instead of a FileFilter object. Repeat with a method expression.
+ */
+public class Exercise2 {
+    public static void main(String[] args) {
+        final String propertyName = "directory.path";
+        final String path = System.getProperty(propertyName);
+
+        if (path == null || path.isEmpty()) {
+            System.out.println(String.format("%s property should be provided", propertyName));
+            return;
+        }
+
+        final File directory = new File(path);
+
+        if (!directory.isDirectory()) {
+            System.out.println(String.format("%s is not a directory", path));
+            return;
+        }
+
+        final List<File> directories1 = Arrays.asList(directory.listFiles(file -> file.isDirectory()));
+        System.out.println("########################################");
+        System.out.println("find directories using lambda expression");
+        System.out.println("########################################");
+        directories1.forEach(System.out::println);
+
+        final List<File> directories2 = Arrays.asList(directory.listFiles(File::isDirectory));
+        System.out.println("#######################################");
+        System.out.println("find directories using method reference");
+        System.out.println("#######################################");
+        directories2.forEach(System.out::println);
+    }
+}
